472b463127fb6a7c4b8a15dd95a9b6d0289a1b40
ykopatko <cs.kopatko.evgen@gmail.com>
2023-09-05 14:36:58 +0300

    Update Database settings to PosrtgeSQL

--- core/settings.py
+++ core/settings.py
@@ -78,11 +78,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["POSTGRES_HOST"],
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
